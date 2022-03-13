589777285fef8a0108a26e30bc5497753bccd944
egoviedo <egoviedoluna@gmail.com>
2022-03-13 16:04:13 -0300

    M04 U04

---
+++ M04/M04U04/app.js
@@ -0,0 +1,90 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var session = require('express-session');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: 'saderfdDErfgty45RdgG',
+  resave: false,
+  saveUninitialized: true
+})
+);
+
+//EJEMPLO Middleware - Contador de vistas
+app.use(function (req, res, next) {  
+   if (!req.session.vistas) {req.session.vistas = {};}
+   if (!req.session.vistas[req.originalUrl]) {
+     req.session.vistas[req.originalUrl] = 1;
+   } else {
+     req.session.vistas[req.originalUrl]++;
+   }
+ next();
+});
+
+app.get('/', function (req, res) {
+  var conocido = Boolean(req.session.nombre);
+  res.render('index', {
+    title: 'Servicios al cliente',
+    conocido: conocido,
+    nombre: req.session.nombre,
+    //sector: req.session.sector,
+    sectorClientes: (req.session.sector=="clientes"),
+    sectorVentas: (req.session.sector=="ventas"),
+    sectorSoporte: (req.session.sector=="tecnico"),
+    serviciosClientes:["Facturación","Consumo" ],
+    serviciosVentas:["Servicio Base","Servicio estandar","Servicio Premiun" ],
+    serviciosSoporte:["Instalación","Comunicacion"],
+    vistas: req.session.vistas[req.originalUrl]    
+  });
+});
+
+app.post('/ingresar', function (req, res) {
+  if (req.body.nombre) {
+    req.session.nombre = req.body.nombre;
+  }
+  if (req.body.sector) {
+    req.session.sector = req.body.sector
+  } 
+
+  res.redirect('/');
+});
+
+app.get('/salir', function (req, res) {
+  req.session.destroy();
+  res.redirect('/');
+});
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
+
+module.exports = app;
